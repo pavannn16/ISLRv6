2279cff97808768a749dbfdc0a73755a78bb703b
pavannn16 <pavannn16@gmail.com>
2025-03-11 10:59:54 +0530

    MaintenanceMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
-import type { AppProps } from 'next/app';
+import { AppProps } from "next/app";
+import Maintenance from "./maintenance";
+import "../styles/globals.css";
 import { inter } from '@/lib/fonts';
 import '@/styles/globals.css';
 import { DayPickerProvider } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
+// Check if the app is in maintenance mode based on environment variable
 function MyApp({ Component, pageProps }: AppProps) {
+  const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "true";
+  
+  // If maintenance mode is active, show maintenance page
+  if (isMaintenanceMode) {
+    return <Maintenance />;
+  }
+  
+  // Otherwise, render the app normally
   return (
     <DayPickerProvider initialProps={{ mode: 'single' }}>
       <Component {...pageProps} />
